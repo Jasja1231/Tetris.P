b94cb6709fc0fec4739f05d64e1120d7b83d2077
Yaryna Konyushenkoy <Yaryna Konyushenkoy>
2016-11-05 18:15:00 +0100

    removed Helper class added Valid comments

--- AlgorithmsGUI/Algorithms/ShapeValidator.cs
+++ AlgorithmsGUI/Algorithms/ShapeValidator.cs
@@ -8,14 +8,17 @@ namespace Tetris
 {
     class ShapeValidator
     {
-        /*
-        0-tile is valid
-        1-tile is hollow
-        2-tile is disconnected
-        */
-        public int isTileValid(Shape shape)
+        /// <summary>
+        /// Checks if shape (tile) is valid, meaning that shape doesn't have any holes and is not disconnected.
+        /// </summary>
+        /// <param name="shape"> shape to be checked for validity</param>
+        /// <returns>int representing wheter the tile is valid or not in the fillowing way : 
+        ///  0-tile is valid
+        ///  1-tile is hollow
+        ///  2-tile is disconnected
+        /// </returns>
+        public int isShapeValid(Shape shape)
         {
-
             return 0;
         }
 
