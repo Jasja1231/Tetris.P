cb82d8b54541f7109ac0a8545bfc8f3e26dad748
cholujm <cholujm@student.mini.pw.edu.pl>
2016-11-29 13:57:45 +0200

    fixed thread test

--- TetrisUnitTests/ThreadWorkTest.cs
+++ TetrisUnitTests/ThreadWorkTest.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tetris.Algorithms;
+using Tetris.Windows;
 
 namespace TetrisUnitTests
 {
@@ -11,26 +12,22 @@ namespace TetrisUnitTests
     public class ThreadWorkTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ThreadTest()
         {
             Model m = new Model();
-            m.BestResults = new List<Result>();
-            m.ShapesDatabase = new Shape[1];
-            byte[,] bytes = {{0, 1}, {0, 1}};
-            m.ShapesDatabase[0] = new Shape(bytes, Color.Black, Color.Black);
-            ThreadComputation tc = new ThreadComputation(m);
-            List<MainTable> lmt = new List<MainTable>();
-            MainTable mainTable = new MainTable(0);
-            mainTable.Width = 3;
-            //In the beginning the height of our tables is equal to the height of the talles Shapes (amount ShapesInfoListWrapper)
-            mainTable.Height = 3;
-            mainTable.Table = new byte[mainTable.Width, mainTable.Height];
-            mainTable.Quantities = new int[] { 1 };
-            //Add it to the list of a Main Tables
-            lmt.Add(mainTable);
-            tc.preformIteration(m,1,lmt);
+            List<byte[,]> lb = new List<byte[,]>();
+            byte[,] bytes = { { 0, 1, 0 }, { 1, 1, 1 }, { 0, 1, 0 }, { 0, 1, 0 } };
+            byte[,] bytes2 = { { 0, 1, 0 }, { 1, 1, 1 }, { 0, 1, 0 }, { 1, 0, 0 } };
+            lb.Add(bytes);
+            lb.Add(bytes2);
+            m.TableWidth = 10;
+            m.ConstructShapes(lb);
+            TileBrowser TB = new TileBrowser(m.AllLoadedShapes);
+            m.ApplyShapes(TB.TileControls);
+            m.StartComputation(1);
             Thread.Sleep(5000);
-            Assert.Equals(m.BestResults.Count, 1);
+            m.StopComputation();
+            Assert.AreEqual(m.BestResults.Count, 1);
         }
     }
 }
