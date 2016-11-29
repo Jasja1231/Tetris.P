22ceae6f872ccecdd690519b90dbf7ce94d9c33a
KKKI <krzysztofiwanczyk5@gmail.com>
2016-11-29 02:01:48 +0000

    .

--- TetrisUnitTests/ValidatorTest.cs
+++ TetrisUnitTests/ValidatorTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Tetris.Algorithms;
+using System.Collections.Generic;
 
 namespace TetrisUnitTests
 {
@@ -8,7 +9,7 @@ namespace TetrisUnitTests
     public class ValidatorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidationTest()
         {
             byte[,]disconnected = new byte[4,4]{{0,0,1,0},{0,1,0,0},{0,0,1,0},{0,0,1,0}};
             byte[,]hole =  new byte[4,4]{{1,1,1,1},{1,0,0,1},{1,0,0,1},{1,1,1,1}};
@@ -20,8 +21,40 @@ namespace TetrisUnitTests
             Assert.AreEqual(0, ShapeValidator.isTileValid(Valid));
             Assert.AreEqual(1, ShapeValidator.isTileValid(Hole));
             Assert.AreEqual(2, ShapeValidator.isTileValid(Disconnected));
-
-
         }
+
+       [TestMethod]
+        public void EqualityTest()
+        {
+            byte[,] arr = new byte[4, 4] { { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 } };
+            byte[,] arr2 = new byte[4, 4] { { 1, 1, 1, 1 }, { 1, 0, 0, 1 }, { 1, 0, 0, 1 }, { 1, 1, 1, 1 } };
+            Shape s1 = new Shape(arr, System.Drawing.Color.FromArgb(0, 0, 0, 0), System.Drawing.Color.FromArgb(0, 0, 0, 0));
+            Shape otherS1 = new Shape(arr, System.Drawing.Color.FromArgb(0, 0, 0, 0), System.Drawing.Color.FromArgb(0, 0, 0, 0));
+            Shape other = new Shape(arr2, System.Drawing.Color.FromArgb(0, 0, 0, 0), System.Drawing.Color.FromArgb(0, 0, 0, 0));
+
+            Assert.IsTrue(ShapeValidator.AreEqual(s1, otherS1));
+            Assert.IsFalse(ShapeValidator.AreEqual(s1, other));
+        }
+
+        [TestMethod]
+        public void DuplicateTest()
+       {
+           var Shapes = new List<Tetris.Controls.TileControl>();
+           byte[,] arr = new byte[4, 4] { { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 }, { 0, 0, 1, 0 } };
+           byte[,] arr2 = new byte[4, 4] { { 1, 1, 1, 1 }, { 1, 0, 0, 1 }, { 1, 0, 0, 1 }, { 1, 1, 1, 1 } };
+           Shape s1 = new Shape(arr, System.Drawing.Color.FromArgb(0, 0, 0, 0), System.Drawing.Color.FromArgb(0, 0, 0, 0));
+           Shape otherS1 = new Shape(arr, System.Drawing.Color.FromArgb(0, 0, 0, 0), System.Drawing.Color.FromArgb(0, 0, 0, 0));
+           Shape other = new Shape(arr2, System.Drawing.Color.FromArgb(0, 0, 0, 0), System.Drawing.Color.FromArgb(0, 0, 0, 0));
+           var tc1 = new Tetris.Controls.TileControl(s1);
+           var tc2 = new Tetris.Controls.TileControl (otherS1);
+           var tc3 = new Tetris.Controls.TileControl (other);
+           Shapes.Add(tc1);
+           Shapes.Add(tc2);
+           Shapes.Add(tc3);
+           ShapeValidator.MarkDuplicates(Shapes);
+           Assert.IsFalse(Shapes[0].IsDuplicate);
+           Assert.IsTrue(Shapes[1].IsDuplicate);
+           Assert.IsFalse(Shapes[2].IsDuplicate);
+       }
     }
 }
