88f36d71aa7afabd0b2b7f2f5545f6fa7361d761
Nibiru-1337 <3psilion@gmail.com>
2016-11-29 01:14:01 +0100

    shape comparer test

--- TetrisUnitTests/ShapeTest.cs
+++ TetrisUnitTests/ShapeTest.cs
@@ -6,16 +6,21 @@ using Tetris;
 namespace TetrisUnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class ShapeTest
     {
         [TestMethod]
         public void ShapeComparisonTest()
         {
             byte[,] bytes = { {0,0,0}, {1,1,1}, {0,0,0} };
+            byte[,] bytes2 = { { 0, 0, 0 }, { 1, 1, 1 }, { 1, 0, 0 } };
             Shape_comparer sc = new Shape_comparer();
             Shape s1 = new Shape(bytes, Color.AliceBlue, Color.AliceBlue);
             Shape s2 = new Shape(bytes, Color.Aqua, Color.Aqua);
-            Assert.Equals(true, sc.Equals(s1, s2));
+            Shape s3 = new Shape(bytes2, Color.Black,Color.Black);
+
+            Assert.AreEqual(true, sc.Equals((Shape)s1, (Shape)s1));
+            Assert.AreEqual(true, sc.Equals((Shape)s1, (Shape)s2));
+            Assert.AreEqual(false, sc.Equals((Shape)s1, (Shape)s3));
         }
     }
 }
